b5c0355c55d777d2dc5c62a1c534b2785957136e
Пряхин Арсений ТСА-141 <ololononoob@gmail.com>
2025-05-30 18:37:09 +0300

    Update 7.cpp

--- 7.cpp
+++ 7.cpp
@@ -93,7 +93,7 @@ void replaceMinAbsWithZero(int** array, size_t m, size_t n);
  * @param n Количество столбцов
  * @param new_n Количество столбцов в новом массиве
  */
-int** removeColumnsWhereFirstGreaterThanLast(int** array, size_t m, size_t n, size_t& new_n);
+int** removeColumnsWhereFirstGreaterThanLast(int** array, size_t m, size_t n);
 /**
  * @brief Расчитывает старый размер массива для удаления столбцов
  * @param array Исходный массив
@@ -101,7 +101,7 @@ int** removeColumnsWhereFirstGreaterThanLast(int** array, size_t m, size_t n, si
  * @param n Количество столбцов
  * @param new_n Количество столбцов в новом массиве
  */
-int** GetCount(int** array, size_t m, size_t n, size_t& new_n);
+int** GetCount(int** array, size_t m, size_t n, size_t new_n);
 
 /**
 * @brief Перечисление для выбора способа заполнения данных
@@ -157,7 +157,7 @@ int main()
     printArray(task1Array, m, n);
     deleteArray(task1Array, m, n);
 
-    size_t& new_n = GetCount();
+    int new_n = n-GetCount();
     int** task2Array = removeColumnsWhereFirstGreaterThanLast(array, m, n, new_n);
     cout << "After Task 2 (removed columns with even first element):";
     cout << " ";
@@ -289,7 +289,7 @@ int main()
         }
     }
 
-    int** GetCount(int** array, size_t m, size_t n, size_t& new_n) {
+    int GetCount(int** array, size_t m, size_t n, int new_n) {
         size_t count = 0;
         for (size_t j = 0; j < n; j++) {
             if (array[0][j] > array[m - 1][j]) {
@@ -297,23 +297,10 @@ int main()
             }
         }
         
-        return nullptr;
+        return count;
     }
 
-    int** removeColumnsWhereFirstGreaterThanLast(int** array, size_t m, size_t n, size_t& new_n) {
-        
-        size_t count = 0;
-        for (size_t j = 0; j < n; j++) {
-            if (array[0][j] > array[m - 1][j]) {
-                count++;
-            }
-        }
-      
-
-        if (new_n == 0) {
-            return nullptr;
-        }
-
+    int** removeColumnsWhereFirstGreaterThanLast(int** array, size_t m, size_t n, int new_n) {
         int** newArray = getNewArray(m, new_n);
         size_t new_j = 0;
         for (size_t j = 0; j < n; j++) {
